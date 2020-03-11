f4f54e790ead9886a2b693b5e267d043ce880b08
Nate Falconer <falconerresources@gmail.com>
2020-03-11 11:38:54 -0400

    components

---
+++ frontend/src/components/main/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <div className="newsFeed">
+                    <h1>News Feed here</h1>
+                </div>
+                <div className="coronaTracker">
+                    <h1>Corona Tracker here</h1>
+                </div>
+                <div className="topHeadlines">
+                    <h1>Top Headlines here</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
