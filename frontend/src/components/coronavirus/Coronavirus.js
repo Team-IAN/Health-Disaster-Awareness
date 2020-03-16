ca6e04c41d0b3e8721ca87dd1a913830eecb514f
Nate Falconer <falconerresources@gmail.com>
2020-03-16 17:51:32 -0400

    changes

--- frontend/src/components/coronavirus/Coronavirus.js
+++ frontend/src/components/coronavirus/Coronavirus.js
@@ -5,7 +5,7 @@ class Coronavirus extends Component {
         return (
             <div>
                 <p>map here</p>
-                <div className="coronaMap"><iframe src="https://www.bing.com/covid" width="1500" height="1000"></iframe></div>
+                <div className="coronaMap"><iframe src="https://hgis.uw.edu/virus/" width="1500" height="1000"></iframe></div>
             </div>
         );
     }
