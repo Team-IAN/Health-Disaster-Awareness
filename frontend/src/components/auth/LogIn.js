9411f5ea1db37739f74ad2422367ed65748a7dde
Igor Zelenov <igorg.zelenov@gmail.com>
2020-03-18 14:15:34 -0400

    login page

--- frontend/src/components/auth/LogIn.js
+++ frontend/src/components/auth/LogIn.js
@@ -1,5 +1,12 @@
 import React, { Component, Fragment } from "react";
 import actions from "../../services/index";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Button
+} from "react-bootstrap";
 
 class LogIn extends Component {
   state = {};
@@ -10,7 +17,7 @@ class LogIn extends Component {
     actions
       .logIn(this.state)
       .then(user => {
-        console.log(user)
+        console.log(user);
         this.props.setUser({ ...user.data });
         this.props.history.push("/home");
       })
@@ -20,7 +27,7 @@ class LogIn extends Component {
     //console.log(this.props)
     return (
       <Fragment>
-        <h2>LogIn</h2>
+        {/* <h2>LogIn</h2>
         <form onSubmit={this.handleSubmit}>
           <div>
             <p>Email</p>
@@ -34,17 +41,45 @@ class LogIn extends Component {
               onChange={this.handleChange}
             />
           </div>
-          {/* <div>
+          <div>
             <p>Location</p>
             <input
               name="location"
               type="location"
               onChange={this.handleChange}
             />
-          </div> */}
+          </div>
           <br></br>
           <input type="submit" value="Login" />
-        </form>
+        </form> */}
+
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Enter Password"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit" value="Login">
+            Log In
+          </Button>
+        </Form>
       </Fragment>
     );
   }
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
     //actions.test()
   }
   render() {
-    return <div></div>
+    return <div></div>;
   }
 }
 
