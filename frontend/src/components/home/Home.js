6aa7a366c4150f17978f5856a26680fbbbdb30ae
Igor Zelenov <igorg.zelenov@gmail.com>
2020-03-19 14:37:37 -0400

    homepage

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,22 +1,58 @@
 import React, { Component } from "react";
 import actions from "../../services/index";
-// import './App.css';
-// import axios from 'axios';
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Col,
+  Row,
+  Button
+} from "react-bootstrap";
 
 class Home extends Component {
   async componentDidMount() {
     //actions.test()
   }
   render() {
-    return <div>
-      <div className="frontTop">
-      <h1>Welcome to the COVID-19 Tracker.</h1>
+    return (
+      <div>
+        <div className="frontTop">
+          <h1>Welcome to the COVID-19 Tracker.</h1>
+        </div>
+        <div className="frontBottom">
+          <h3>
+            Get the latest Coronavirus information here, tailored to your
+            location.
+          </h3>
+          <h4>
+            Like your favorite articles and check the User Feed to see what
+            other users have Liked.
+          </h4>
+        </div>
+
+        <Form.Row>
+          <Col>
+            <Form.Control
+              name="email"
+              class="quick-signup-email"
+              type="email"
+              placeholder="Email"
+            />
+          </Col>
+          <Col>
+            <Button
+              className="btn-md btd-dark btn-block btn-success"
+              variant="primary"
+              type="submit"
+              href="/sign-up"
+            >
+              Sign Up
+            </Button>
+          </Col>
+        </Form.Row>
       </div>
-      <div className="frontBottom">
-        <h3>Get the latest Coronavirus information here, tailored to your location.</h3>
-        <h4>Like your favorite articles and check the User Feed to see what other users have Liked.</h4>
-      </div>
-    </div>;
+    );
   }
 }
 
--- frontend/src/components/prevention/Prevention.js
+++ frontend/src/components/prevention/Prevention.js
@@ -1,53 +1,57 @@
-import React, { Component, Fragment } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import CarouselPage from '../carousel/CarouselPage'
-import YouTube from 'react-youtube';
-import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
-  "mdbreact";
+import React, { Component, Fragment } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import CarouselPage from "../carousel/CarouselPage";
+import YouTube from "react-youtube";
+import {
+  MDBCarousel,
+  MDBCarouselCaption,
+  MDBCarouselInner,
+  MDBCarouselItem,
+  MDBView,
+  MDBMask,
+  MDBContainer
+} from "mdbreact";
 
 class Prevention extends Component {
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
 
-    _onReady(event) {
-        // access to player in all event handlers via event.target
-        event.target.pauseVideo();
-      }
-    
-    render() {
-        const opts = {
-            height: '676',
-            width: '1110',
-            maxWidth: '100%'
-        }
-        return (
-            <Fragment>
-                <div className='something'>
+  render() {
+    const opts = {
+      height: "676",
+      width: "1110",
+      maxWidth: "100%"
+    };
+    return (
+      <Fragment>
+        <div className="something"></div>
+        <div>
+          <CarouselPage />
+        </div>
+        <div className="something"></div>
+        <div>
+          <h1>
+            <center>
+              [LIVE] Coronavirus Pandemic: Real Time Counter, World Map, News
+            </center>
+          </h1>
 
-                </div>
-                <div>
-                    <CarouselPage />
-                </div>
-                <div className='something'>
-
-                </div>
-                <div>
-                <h1><center>[LIVE] Coronavirus Pandemic: Real Time Counter, World Map, News</center></h1>
-
-<br></br>
-                <MDBContainer>
-                    <YouTube
-                        className='videoContainer'
-                        videoId="qgylp3Td1Bw"
-                        opts={opts}
-                        onReady={this._onReady}
-                    />
-                </MDBContainer>
-                </div>
-                <div className='something'>
-
-</div>
-            </Fragment>
-        );
-    }
+          <br></br>
+          <MDBContainer>
+            <YouTube
+              className="videoContainer"
+              videoId="qgylp3Td1Bw"
+              opts={opts}
+              onReady={this._onReady}
+            />
+          </MDBContainer>
+        </div>
+        <div className="something"></div>
+      </Fragment>
+    );
+  }
 }
 
 export default Prevention;
