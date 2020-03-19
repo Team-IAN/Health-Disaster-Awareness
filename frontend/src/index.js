d415b44f2a810f10bdfa93b46de186aff6977bd5
Nate Falconer <falconerresources@gmail.com>
2020-03-19 18:44:09 -0400

    nav

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'rsuite/lib/styles/index.less'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
